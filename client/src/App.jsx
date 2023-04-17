e20e9cb29b82e82ab529864139230054437252f3
zainab nassar <zsn002@pu.edu.lb>
2023-04-18 00:09:48 +0300

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {Route,Routes} from "react-router-dom";
 import './App.css'
 import Layout from "./Layout";
+import AccountPage from "./pages/AccountPage";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -16,6 +17,10 @@ function App() {
       <Route index element={<IndexPage />}/>
       <Route path="/login" element={<LoginPage />}/>
       <Route path="/register" element={<RegisterPage />}/>
+      <Route path="/account/:subpage?" element={<AccountPage />}/>
+      <Route path="/account/:subpage/:action" element={<AccountPage />}/>
+      {/* <Route path="/account/history" element={<AccountPage />}/>
+      <Route path="/account/VandE" element={<AccountPage />}/> */}
       </Route>
      
     </Routes>
